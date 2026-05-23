030e1a02dbd20100ef5c0284c36f7d5a17a591c4
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-23 11:14:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,98 +1,28 @@
 import streamlit as st
 
-
-
 st.set_page_config(
-    page_title="Finance Dashboard",
-    layout="wide"
+  page_title="Kuliah Praktisi 2305",
+  page_icon="🧊",
+  layout="centered",
+  initial_sidebar_state="expanded"
 )
 
-option = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
-)
-
-# Dummy database user
-USERS = {
-    "admin": "12345",
-    "user": "abcde"
-}
-
-# Dummy chatbot
-def finance_bot(prompt):
-    return f"FinanceBot menjawab: {prompt}"
-
-# Session state
-if "authenticated" not in st.session_state:
-    st.session_state.authenticated = False
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# UI
+# Hirarki teks
 st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
+st.write("Hello, *World!* 😁")
 
-# Login
-username = st.text_input("Username")
-password = st.text_input("Password", type="password")
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
 
-if st.button("Login"):
-    if USERS.get(username) == password:
-        st.session_state.authenticated = True
-        st.session_state.username = username
-        st.success("Login berhasil")
-        st.rerun()
-    else:
-        st.error("Invalid credentials")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
 
-# Setelah login
-if st.session_state.authenticated:
-
-    st.write(f"Selamat datang, {st.session_state.username}")
-
-    st.chat_message("assistant").write(
-        "Hi! Saya FinanceBot."
-    )
-
-    # Tampilkan history
-    for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(
-            msg["content"]
-        )
-
-    # Input chat
-    prompt = st.chat_input(
-        "Tulis pertanyaan Anda..."
-    )
-
-    if prompt:
-
-        # Simpan user message
-        st.session_state.messages.append(
-            {
-                "role": "user",
-                "content": prompt
-            }
-        )
-
-        st.chat_message("user").write(prompt)
-
-        # Bot response
-        response = finance_bot(prompt)
-
-        st.session_state.messages.append(
-            {
-                "role": "assistant",
-                "content": response
-            }
-        )
-
-        st.chat_message("assistant").write(response)
-
-import streamlit as st
+st.button("Reset", type="primary")
 
 col1, col2, col3 = st.columns(3)
 
@@ -107,6 +37,13 @@ with col2:
 with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
-with col3:
-    st.header("Fitur 3")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
