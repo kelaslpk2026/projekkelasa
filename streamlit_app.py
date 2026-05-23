17cb8f97aa67d770e254d8740af172bad4540d22
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-23 09:53:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,40 @@
 import streamlit as st
-# app.py — baris 13
+
 st.set_page_config(
-  page_title="Finance Dashboard",
-  layout="wide"
+    page_title="Finance Dashboard",
+    layout="wide"
 )
 
-# Hirarki teks
+# Dummy database user
+USERS = {
+    "admin": "12345",
+    "user": "abcde"
+}
+
+# UI
 st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
-# Inisialisasi session_state
+
+# Session state
 if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 
-# Login check
+# Input login
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+# Login button
 if st.button("Login"):
     if USERS.get(username) == password:
         st.session_state.authenticated = True
         st.session_state.username = username
+        st.success("Login berhasil")
         st.rerun()
     else:
         st.error("Invalid credentials")
+
+# Setelah login
+if st.session_state.authenticated:
+    st.write(f"Selamat datang, {st.session_state.username}")
