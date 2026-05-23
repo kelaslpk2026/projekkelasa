5739f8855815ac47ac3f81edf69b81d732a9f308
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-23 10:39:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Fitur 1")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("Fitur 2")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("Fitur 3")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
 
 st.set_page_config(
     page_title="Finance Dashboard",
