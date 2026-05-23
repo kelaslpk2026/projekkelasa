9ea8c7c4f655b9510a3bea4476fb9099823a1a0d
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-23 09:33:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,11 @@
-import streamlit as st
+# app.py — baris 13
+st.set_page_config(
+  page_title="Finance Dashboard",
+  layout="wide"
+)
 
-st.title(":blue[Kalkulator Aritmatika Kelas A]:rocket:")
-st.header("*Kalkulator*  **Aritmatika** ***Sederhana***.")
-number = st.number_input("Masukkan Angka",min_value=0)
-satu,dua,tiga,empat,lima=st.columns(5)
-if satu.button("Faktorial"):
-    number = int(number)
-    st.subheader(math.factorial(number))
-elif dua.button("Akar Kuadrat"):
-    st.subheader(math.sqrt(number))
-elif tiga.button("Kuadrat") :
-    st.subheader(number**2)
-elif empat.button("Logaritma") :
-    st.subheader(math.log(number))
-elif lima.button("Ln"):
-    st.subheader(math.ln(number))
-if st.button("reset"):
-    st.rerun()
+# Hirarki teks
+st.title("📊 Dashboard")
+st.header("Laporan Bulanan")
+st.subheader("📈 Monthly Expenses")
+st.caption("Made with ❤️ using Streamlit")
